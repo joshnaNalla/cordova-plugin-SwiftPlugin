408b735893fc8b351b6b5a7d53fa394024c3a18d
joshnaNalla <32710126+joshnaNalla@users.noreply.github.com>
2019-12-17 18:25:49 +0530

    Update YourPluginName.js

--- www/YourPluginName.js
+++ www/YourPluginName.js
@@ -1,1 +1,12 @@
+var exec = require('cordova/exec');
 
+var PLUGIN_NAME = "SwiftPlugin"; // This is just for code completion uses.
+
+var SwiftPlugin = function() {}; // This just makes it easier for
+ us to export all of the functions at once.
+// All of your plugin functions go below this. 
+// Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
+SwiftPlugin.callFramework = function(onSuccess, onError) {
+   exec(onSuccess, onError, PLUGIN_NAME, "callFramework", []);
+};
+module.exports = SwiftPlugin;
